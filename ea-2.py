2a2ed1959b2c8ca14b40cee6a73ad8c05a9cc311
mattiejas <mattiejas@outlook.com>
2020-12-06 19:11:18 +0100

    kill me

--- ea-2.py
+++ ea-2.py
@@ -1,5 +1,3 @@
-
-
 from IOHexperimenter import IOH_function, IOH_logger, IOHexperimenter
 import numpy as np
 import math
@@ -26,27 +24,41 @@ class EvolutionStrategy():
         self.one_sig = 1
 
     def optimize(self, problem):
+        # Initiliaze required variables
+        self.n = problem.number_of_variables
+
         self.t0 = 1 / math.sqrt(problem.number_of_variables)
 
-        # Initiliaze required variables
-        n = problem.number_of_variables
+        self.tau = 1 / math.sqrt(2 * math.sqrt(self.n))
+        self.tau_prime = 1 / math.sqrt(2 * self.n)
+
+        self.beta = math.pi / 36
 
         fopt = -sys.maxsize-1
         x_prime = None
 
         # Generate initial population
-        population = np.random.uniform(
-            low=-5, high=5, size=(self.population_size, n))
+        population = np.random.uniform(low=-5, high=5, size=(self.population_size, self.n))
         # Add the one sigmas
-        sigmas = np.ones(len(population))
-        population = np.append(population, np.array([sigmas]).T, axis=1)
+
+        if self.mutation_type == 1:
+            sigmas = np.ones(len(population))
+            population = np.append(population, np.array([sigmas]).T, axis=1)
+
+        if self.mutation_type == 2:
+            sigmas = np.ones(population.shape)
+            population = np.append(population, sigmas, axis=1)    
+            
+        if self.mutation_type == 3:
+            sigmas = np.ones((population.shape[0], population.shape[1] + self.n*(self.n-1)//2))
+            population = np.append(population, sigmas, axis=1)
 
         performance = self.eval_population(population, problem)
 
         # !! final_target_hit returns True if the optimum has been found.
         # !! evaluations returns the number of function evaluations has been done on the problem.
 
-        while not problem.final_target_hit and problem.evaluations < self.budget_base * n:
+        while not problem.final_target_hit and problem.evaluations < self.budget_base * self.n:
             # Recombination
             recombined = self.recombine(population, 5)
 
@@ -57,11 +69,10 @@ class EvolutionStrategy():
             evaluated = self.eval_population(mutated, problem)
 
             # Selection
-            population, performance = self.selection(
-                population, mutated, performance, evaluated)
+            population, performance = self.selection(population, mutated, performance, evaluated)
 
             x, f = self.find_best(population, performance)
-            if f >= fopt:
+            if f < fopt:
                 x_prime = x
                 fopt = f
 
@@ -80,9 +91,13 @@ class EvolutionStrategy():
     def mutate(self, population):
         # T0 = 1 / sqrt(n) with n = dimension?
         if self.mutation_type == 0:
-            return self.one_sigma(population)
+            return self.global_sigma(population)
         if self.mutation_type == 1:
-            return self.individual_sigma(population)
+            return self.one_sigma(population)
+        if self.mutation_type == 2:
+            return self.individual_sigma(population)        
+        if self.mutation_type == 3:
+            return self.correlated_sigma(population)
 
     def selection(self, parents, offspring, p_eval, o_eval):
         if self.selection_type == 0:  # (u, l)-selection
@@ -90,18 +105,72 @@ class EvolutionStrategy():
         if self.selection_type == 1:  # (u + l)-selection
             return self.select_tournament(np.append(parents, offspring, axis=0), np.append(p_eval, o_eval, axis=0))
 
-    def one_sigma(self, population):
-        # Update one_sigma for entire population
+    def global_sigma(self, population):
+        # Update global_sigma for entire population
         self.one_sig = self.one_sig * math.exp(np.random.normal(0, self.t0))
-
+        print(self.one_sig)
         population += np.random.normal(0, self.one_sig, population.shape)
         return population
 
-    def individual_sigma(self, population):
-        population[:, -1] = population[:, -1] * math.exp(np.random.normal(0, self.t0))
+    def one_sigma(self, population):
+        for y in range(population.shape[0]):
+            # update sigma
+            population[y, -1] = population[y, -1] * np.exp(np.random.normal(0, self.t0))
+            print(population[y, -1])
 
-        population[:, :-1] += np.random.normal(0, population[:, -1], (population.shape[0], population.shape[1] - 1))
+            # mutate
+            population[y, :-1] += np.random.normal(0, population[y, -1], population.shape[1] - 1)
         return population
+
+    def individual_sigma(self, population):
+        for y in range(population.shape[0]):
+            step_size = np.random.normal(0, self.tau_prime)
+            for i in range(population.shape[1]//2):
+                # update sigmas
+                population[y, population.shape[1]//2+i] *= math.exp(step_size + np.random.normal(0, self.tau))
+                print(population[y, population.shape[1]//2+i])
+                # mutate
+                population[y, i] += np.random.normal(0, population[y, population.shape[1]//2 + i])
+        return population   
+        
+    def correlated_sigma(self, population):
+        for y in range(population.shape[0]):
+            step_size = np.random.normal(0, self.tau_prime)
+
+            # update sigmas
+            for i in range((population.shape[1] - (self.n*(self.n-1)//2))//2):
+                population[y, (population.shape[1] - (self.n*(self.n-1)//2))//2 + i] *= math.exp(step_size + np.random.normal(0, self.tau))
+
+            # mutation of rotation angles
+            for j in range(self.n*(self.n-1)//2):
+                population[y, (population.shape[1] - (self.n*(self.n-1)//2)) + j] += np.random.normal(0, self.beta)
+                if population[y, (population.shape[1] - (self.n*(self.n-1)//2)) + j] > math.pi:
+                    population[y, (population.shape[1] - (self.n*(self.n-1)//2)) + j] = population[y, (population.shape[1] - (self.n*(self.n-1)//2)) + j] - 2 * math.pi \
+                        * np.sign(population[y, (population.shape[1] - (self.n*(self.n-1)//2)) + j])
+
+            uncorrelated_sigmas = [] 
+            for s in population[y, ((population.shape[1] - (self.n*(self.n-1))//2)//2):(population.shape[1] - (self.n*(self.n-1)//2))]:
+                uncorrelated_sigmas.append(np.random.normal(0, math.sqrt(s)))
+            print(uncorrelated_sigmas)
+
+            alphas = population[y, (population.shape[1] - (self.n*(self.n-1)//2)):]
+            nq = (self.n*(self.n-1)//2) - 1
+            for k in range(1, self.n):
+                n1 = self.n - k - 1
+                n2 = self.n - 1
+                for i in range(1, k + 1):
+                    d1 = uncorrelated_sigmas[n1]
+                    d2 = uncorrelated_sigmas[n2]
+
+                    uncorrelated_sigmas[n2] = d1 * np.sin(alphas[nq]) + d2 * np.cos(alphas[nq])
+                    uncorrelated_sigmas[n1] = d1 * np.cos(alphas[nq]) - d2 * np.sin(alphas[nq])
+
+                    n2 -= 1
+                    n1 -= 1
+
+            for i, s in enumerate(uncorrelated_sigmas):
+                population[y, i] += np.random.normal(0, np.abs(s))
+        return population    
 
     def intermediate_recombination(self, population, n_offspring):
         offsprings = []
@@ -162,7 +231,7 @@ class EvolutionStrategy():
             a = tournament[np.random.randint(0, high=len(population))]
             b = tournament[np.random.randint(0, high=len(population))]
 
-            winning = [a, b][np.argmax([a[1], b[1]])]
+            winning = [a, b][np.argmin([a[1], b[1]])]
 
             winners.append(winning[0])
             performances.append(winning[1])
@@ -170,7 +239,13 @@ class EvolutionStrategy():
         return np.array(winners), np.array(performances)
 
     def eval_population(self, population, problem):
-        return np.array([problem(x[:-1]) for x in population])
+        if self.mutation_type == 1:
+            return np.array([problem(x[:-1]) for x in population])
+        if self.mutation_type == 2:
+            return np.array([problem(x[:population.shape[1]//2]) for x in population])
+        if self.mutation_type == 3:
+            return np.array([problem(x[:(population.shape[1]-self.n*(self.n-1)//2)//2]) for x in population])
+        return np.array([problem(x) for x in population])
 
     def find_best(self, population, performance):
         idx = np.argmax(performance)
@@ -185,8 +260,10 @@ class EvolutionStrategy():
 #   0: (u, l)
 #   1: (u + l)
 # Mutation types
-#   0: one_sigma
-#   1: individual_sigma
+#   0: global_sigma
+#   1: one_sigma
+#   2: individual_sigma
+#   3: correlated
 # Recombination types
 #   0: global_intermediate_recombination
 #   1: intermediate_recombination
@@ -227,8 +304,10 @@ def aarnoutse_witte_ES(problem):
 #   0: (u, l)
 #   1: (u + l)
 # Mutation types
-#   0: one_sigma
-#   1: individual_sigma
+#   0: global_sigma
+#   1: one_sigma
+#   2: individual_sigma
+#   3: correlated
 # Recombination types
 #   0: global_intermediate_recombination
 #   1: intermediate_recombination
@@ -238,12 +317,13 @@ def aarnoutse_witte_ES(problem):
 
 def experiment():
     problem_id = range(1, 2)
-    instance_id = range(1, 5)
+    instance_id = range(1, 2)
     dimension = [2, 5, 20]
 
-    configurations_space = [[10, 0, 0, 0], [10, 1, 0, 0]]
-    logger = IOH_logger("./", "result", f"aarnoutse_witte", f"aarnoutse_witte")
+    configurations_space = [[10, 1, 3, 1], [10, 1, 0, 0]]
     for index, configuration in enumerate(configurations_space):
+        logger = IOH_logger(
+            "./", "result", f"aarnoutse_witte-{', '.join(map(str, configuration))}", f"aarnoutse_witte-{', '.join(map(str, configuration))}")
         for p_id in problem_id:
             for d in dimension:
                 print(f' problem: {p_id}, dim: {d}')
@@ -253,7 +333,8 @@ def experiment():
                     f.add_logger(logger)
                     es = EvolutionStrategy(configuration)
                     xopt, fopt = es.optimize(f)
-    logger.clear_logger()
+        logger.clear_logger()
 
 
-experiment()
+if __name__ == '__main__':
+    experiment()
